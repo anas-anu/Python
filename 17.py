dcc08437da80c78040e2d90cde7709bd9a0d0b7a
MOHAMMED ANAS <121112953+anas-anu@users.noreply.github.com>
2023-01-06 00:04:49 +0530

    Delete 17.py

--- 17.py
+++
@@ -1,6 +0,0 @@
-d={"YAZIM":10,"CUT GOPAN":20,"LIYAKATHU":30,"SHAHABAS":40,"AJU":50,"KOTTARATHIL":60,"JASEEM":70,"JAISON":80}
-#sorted is a method in dictionary in to sort
-sort=sorted(d.items())
-print(sort)
-sort2=sorted(d.items(),reverse=True)
-print(sort2)
